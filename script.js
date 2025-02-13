4879c6137cf6a21d0ec75520067b1356d72b960d
Ujjwal9329 <ujjwalgujral8@gmail.com>
2025-02-13 12:49:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 document.getElementById("fname").onblur = function() {
     this.value = this.value.toUpperCase();
 };
+
+const val = document.getElementById("fname")
+
+val.addEventListener("blur" , ()=>{
+	let val = input.value;
+	input.value =val.toUpperCase();
+	
+})
